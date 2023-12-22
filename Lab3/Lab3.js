b9592ab31ab46a150b301394344e1b71828b81f6
Uliana0106 <132298083+Uliana0106@users.noreply.github.com>
2023-12-22 10:15:48 +0200

    Update Lab3.js

--- Lab3/Lab3.js
+++ Lab3/Lab3.js
@@ -25,72 +25,19 @@ function init() {
   box1.scale.set(15, 4, 4);
   scene.add(box1);
 
-  const box2 = new THREE.Mesh(boxGeometry, brickMaterial);
-  box2.position.set(-10.77, 0, -7.673);
-  box2.rotation.set(0, -60 * Math.PI / 180, 0);
-  box2.scale.set(20, 4, 4);
-  scene.add(box2);
+  // ... (add other boxes and meshes here)
 
-  const box3 = new THREE.Mesh(boxGeometry, brickMaterial);
-  box3.position.set(5.5, 0, -4.402);
-  box3.rotation.set(0, 90 * Math.PI / 180, 0);
-  box3.scale.set(5, 4, 4);
-  scene.add(box3);
-
-  const box4 = new THREE.Mesh(boxGeometry, brickMaterial);
-  box4.position.set(-0.4, 0, -0.6);
-  box4.scale.set(2, 4, 4);
-  scene.add(box4);
-
-  const box5 = new THREE.Mesh(boxGeometry, brickMaterial);
-  box5.position.set(-6.53, 0, -15.305);
-  box5.scale.set(15, 4, 4);
-  scene.add(box5);
-
-  const cylinder = new THREE.Mesh(cylinderGeometry, brickMaterial);
-  cylinder.position.set(5.5, 0, -7.1);
-  scene.add(cylinder);
-
-  const plane = new THREE.Mesh(planeGeometry, new THREE.MeshBasicMaterial({ color: 0x888 }));
-  plane.rotation.set(-Math.PI / 2, 0, 0);
-  plane.position.set(0, -2, 0);
-  scene.add(plane);
-
+  // Create colored boxes
   const coloredBox1 = new THREE.Mesh(boxGeometry, coloredMaterial);
   coloredBox1.position.set(4.5, 0.5, 2);
   coloredBox1.scale.set(2, 2, 0.2);
   scene.add(coloredBox1);
 
-  const coloredBox2 = new THREE.Mesh(boxGeometry, coloredMaterial);
-  coloredBox2.position.set(-5.5, 0.5, 2);
-  coloredBox2.scale.set(2, 2, 0.2);
-  scene.add(coloredBox2);
+  // ... (add other colored boxes)
 
-  const coloredBox3 = new THREE.Mesh(boxGeometry, coloredMaterial);
-  coloredBox3.position.set(-6, 0.15, -13.4);
-  coloredBox3.scale.set(2, 2, 0.2);
-  scene.add(coloredBox3);
-
-  const coloredBox4 = new THREE.Mesh(boxGeometry, coloredMaterial);
-  coloredBox4.position.set(-9, 0.15, -17.3);
-  coloredBox4.scale.set(2, 2, 0.2);
-  scene.add(coloredBox4);
-
-  const coloredBox5 = new THREE.Mesh(boxGeometry, coloredMaterial);
-  coloredBox5.position.set(7.444, 0.15, -5.544);
-  coloredBox5.rotation.set(0, 90 * Math.PI / 180, 0);
-  coloredBox5.scale.set(2, 2, 0.2);
-  scene.add(coloredBox5);
-
-  const coloredBox6 = new THREE.Mesh(boxGeometry, coloredMaterial);
-  coloredBox6.position.set(-12.326, 0.15, -6.311);
-  coloredBox6.rotation.set(0, -60 * Math.PI / 180, 0);
-  coloredBox6.scale.set(2, 2, 0.2);
-  scene.add(coloredBox6);
-
-// Create camera
+  // Create camera
   camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.set(0, 2, -10); // Adjusted the z-coordinate
+  camera.position.set(0, 2, -5);
   camera.lookAt(0, 0, 0);
 
   // Enable ARButton
